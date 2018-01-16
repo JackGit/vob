ac06026c67931c42929349b315bb37e1ad45c3d6
ygjack <ygjack414@hotmail.com>
2018-01-16 13:37:16 +0800

    cp some vue files

---
+++ src/observer/index.js
@@ -0,0 +1,49 @@
+import Dep from './dep'
+
+export function defineReactive (obj, key, val) {
+  const dep = new Dep()
+  const property = Object.getOwnPropertyDescriptor(obj, key)
+
+  if (property && property.configurable === false) {
+    return
+  }
+
+  // cater for pre-defined getter/setters
+  const getter = property && property.get
+  const setter = property && property.set
+
+  let childOb = observe(val)
+  
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function reactiveGetter () {
+      const value = getter ? getter.call(obj) : val
+      if (Dep.target) {
+        dep.depend()
+        if (childOb) {
+          childOb.dep.depend()
+          if (Array.isArray(value)) {
+            dependArray(value)
+          }
+        }
+      }
+      return value
+    },
+    set: function reactiveSetter (newVal) {
+      const value = getter ? getter.call(obj) : val
+
+      if (newVal === value || (newVal !== newVal && value !== value)) {
+        return
+      }
+
+      if (setter) {
+        setter.call(obj, newVal)
+      } else {
+        val = newVal
+      }
+      childOb = !shallow && observe(newVal)
+      dep.notify()
+    }
+  })
+}
